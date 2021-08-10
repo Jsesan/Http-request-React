7918513e9b4f88cd7a6232dab81a5da6e13ac811
Jsesan <jsesan00@gmail.com>
2021-08-10 08:32:07 -0300

    using async and await instead of .then()

--- src/App.js
+++ src/App.js
@@ -6,20 +6,20 @@ import "./App.css";
 function App() {
   const [movies, setMovies] = useState([]);
 
-  function fetchMoviesHandler() {
-    fetch("https://swapi.dev/api/films/")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        const transformedMovies = data.results.map(movie => {return {
-          id: movie.episode_id,
-          title: movie.title,
-          openingText: movie.opening_crawl,
-          releaseDate: movie.release_date
-        }});
-        setMovies(transformedMovies);
-      });
+  async function fetchMoviesHandler() {
+    const res = await fetch("https://swapi.dev/api/films/");
+
+    const data = await res.json();
+
+    const transformedMovies = data.results.map((movie) => {
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        releaseDate: movie.release_date,
+      };
+    });
+    setMovies(transformedMovies);
   }
 
   return (
